f6ae9fe6911c5ba4606af79539fcbf34a1da6c33
Mike <mblack101@ufl.edu>
2019-12-04 18:44:41 -0500

    Add starter files and add part of initial logic for game

---
+++ main.js
@@ -0,0 +1,64 @@
+// Stage 1 - User clicks a color and the game does an action the number of times the name of the color is long • 
+// Stage 2 - User clicks a number and the game does an action that many times • 
+// Stage 3 - User clicks a number and a random fortune appears
+
+
+// Choose a color
+// four bl
+// four colors
+// eight numbers 
+// (2 sets of 4)
+// ten fortunes
+
+
+class FortuneTeller {
+    constructor (colors, fortunes) {
+this.colors = colors;
+this.fortunes = fortunes;
+}
+
+doColorLoop () {};
+
+doNumberLoop () {};
+doFinalChoice () {};
+
+addEventColorLoop
+
+addEventNumberLoop () {};
+
+addEventFinalChoice
+
+
+do
+
+
+
+
+
+}
+
+
+
+
+const FORTUNES = {};
+
+
+
+var dict = {};
+
+dict['key'] = "testing";
+
+let fortuneTeller = new FortuneTeller(COLORS)
+
+
+// for each letter in color, do some animation
+
+// Pick a number
+
+// loop through some actions the amount of times as the number
+
+// pick another number
+
+// fortune is revealed
+
+
